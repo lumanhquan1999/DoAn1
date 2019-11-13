a957384462d80963be700d63f84d29f44dec33df
lumanhquan1999 <43359257+lumanhquan1999@users.noreply.github.com>
2019-11-13 14:37:06 +0700

    Update ThemSach.cs

--- Đồ án 1/ThemSach.cs
+++ Đồ án 1/ThemSach.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -23,7 +23,7 @@ namespace Đồ_án_1
         {
             if (tMaSach.Text.CompareTo("") == 0) ktThem = 1;
             else
-                temp.Nhap(tMaSach.Text, tTenSach.Text, tLoaiSach.Text, tDonGia.Text);
+                temp.Nhap(tMaSach.Text, tTenSach.Text, tLoaiSach.Text, Convert.ToInt32(tDonGia.Text));
             this.Close();
         }
     }
