8f5c07ad9f74d309fdd8dcd715c85dd17d996bdf
lumanhquan1999 <43359257+lumanhquan1999@users.noreply.github.com>
2019-11-13 14:37:41 +0700

    Update XoaSach.Designer.cs

--- Đồ án 1/XoaSach.Designer.cs
+++ Đồ án 1/XoaSach.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Đồ_án_1
+namespace Đồ_án_1
 {
     partial class XoaSach
     {
@@ -30,32 +30,47 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.tMaSach = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(286, 249);
+            this.button1.Location = new System.Drawing.Point(371, 231);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 39);
+            this.button1.Size = new System.Drawing.Size(156, 48);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // tMaSach
             // 
-            this.tMaSach.Location = new System.Drawing.Point(250, 91);
+            this.tMaSach.Location = new System.Drawing.Point(403, 132);
+            this.tMaSach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tMaSach.Name = "tMaSach";
-            this.tMaSach.Size = new System.Drawing.Size(135, 20);
+            this.tMaSach.Size = new System.Drawing.Size(179, 22);
             this.tMaSach.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã sách";
             // 
             // XoaSach
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tMaSach);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "XoaSach";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -65,6 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox tMaSach;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox tMaSach;
     }
 }
